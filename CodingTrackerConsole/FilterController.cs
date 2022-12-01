2b83533f207f250796b19065f1a8c890d0ee420f
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-01 15:52:10 +0000

    Add a GetFilter method to get user input dynamically

--- CodingTrackerConsole/FilterController.cs
+++ CodingTrackerConsole/FilterController.cs
@@ -10,9 +10,10 @@ public class FilterController
         Console.WriteLine("y to filter by years");
     }
 
-    public List<CodingTrackerModel> Filter(List<CodingTrackerModel> dataList, string filter)
+    public List<CodingTrackerModel> Filter(List<CodingTrackerModel> dataList)
     {
         var output = new List<CodingTrackerModel>();
+
         ShowFilterMenu();
         var choice = _input.GetChoice();
         while (choice != "b")
@@ -20,14 +21,11 @@ public class FilterController
             switch (choice)
             {
                 case "d":
-                    output = FilterByDays(dataList, filter);
-                    break;
+                    return FilterByDays(dataList, choice);
                 case "m":
-                    output = FilterByMonths(dataList, filter);
-                    break;
+                    return FilterByMonths(dataList, choice);
                 case "y":
-                    output = FilterByYears(dataList, filter);
-                    break;
+                    return FilterByYears(dataList, choice);
                 default:
                     Console.WriteLine("Wrong input!");
                     break;
@@ -40,22 +38,40 @@ public class FilterController
         return output;
     }
 
-    private List<CodingTrackerModel> FilterByDays(List<CodingTrackerModel> dataList, string filter)
+    private string GetFilter(string choice)
     {
+        switch (choice)
+        {
+            case "d":
+                return _input.GetDay();
+            case "m":
+                return _input.GetMonth();
+            case "y":
+                return _input.GetYear();
+            default:
+                return "00";
+        }
+    }
+
+    private List<CodingTrackerModel> FilterByDays(List<CodingTrackerModel> dataList, string choice)
+    {
+        var filter = GetFilter(choice);
         var output = dataList.Where(x => x.Date!.Substring(3, 2) == filter).ToList();
 
         return output;
     }
 
-    private List<CodingTrackerModel> FilterByMonths(List<CodingTrackerModel> dataList, string filter)
+    private List<CodingTrackerModel> FilterByMonths(List<CodingTrackerModel> dataList, string choice)
     {
+        var filter = GetFilter(choice);
         var output = dataList.Where(x => x.Date!.Substring(0, 2) == filter).ToList();
 
         return output;
     }
 
-    private List<CodingTrackerModel> FilterByYears(List<CodingTrackerModel> dataList, string filter)
+    private List<CodingTrackerModel> FilterByYears(List<CodingTrackerModel> dataList, string choice)
     {
+        var filter = GetFilter(choice);
         var output = dataList.Where(x => x.Date!.Substring(6, 4) == filter).ToList();
 
         return output;
